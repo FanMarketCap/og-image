b4cabe201c84cffd66d481e8de13a7042f00aa97
Mustafa Turhan <mustaphaturhan@gmail.com>
2022-01-05 14:00:04 +0300

    add user og

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -1,60 +1,78 @@
-import { IncomingMessage } from 'http';
-import { parse } from 'url';
-import { ParsedRequest, Theme } from './types';
+import { IncomingMessage } from "http";
+import { parse } from "url";
+import { ParsedRequest, Theme } from "./types";
 
 export function parseRequest(req: IncomingMessage) {
-    console.log('HTTP ' + req.url);
-    const { pathname, query } = parse(req.url || '/', true);
-    const { price, l24, h24, images, widths, heights, md, marketcap, change_24h } = (query || {});
+  console.log("HTTP " + req.url);
+  const { pathname, query } = parse(req.url || "/", true);
+  const {
+    price,
+    l24,
+    h24,
+    images,
+    widths,
+    heights,
+    md,
+    marketcap,
+    change_24h,
+    username,
+    alerts,
+  } = query || {};
 
-    const arr = (pathname || '/').slice(1).split('.');
-    let extension = '';
-    let text = '';
-    if (arr.length === 0) {
-        text = '';
-    } else if (arr.length === 1) {
-        text = arr[0];
-    } else {
-        extension = arr.pop() as string;
-        text = arr.join('.');
-    }
+  const arr = (pathname || "/").slice(1).split(".");
+  let extension = "";
+  let text = "";
+  if (arr.length === 0) {
+    text = "";
+  } else if (arr.length === 1) {
+    text = arr[0];
+  } else {
+    extension = arr.pop() as string;
+    text = arr.join(".");
+  }
 
-    const parsedRequest: ParsedRequest = {
-        fileType: extension === 'jpeg' ? extension : 'png',
-        text: decodeURIComponent(text),
-        theme: 'light',
-        md: md === '1' || md === 'true',
-        price: price || '-',
-        l24: l24 || '-',
-        h24: h24 || '-',
-        marketcap: marketcap || '-',
-        change_24h: change_24h || '-',
-        images: getArray(images),
-        widths: getArray(widths),
-        heights: getArray(heights),
-    };
-    parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
-    return parsedRequest;
+  const parsedRequest: ParsedRequest = {
+    fileType: extension === "jpeg" ? extension : "png",
+    text: decodeURIComponent(text),
+    theme: "light",
+    md: md === "1" || md === "true",
+    price: price || "-",
+    l24: l24 || "-",
+    h24: h24 || "-",
+    marketcap: marketcap || "-",
+    change_24h: change_24h || "-",
+    images: getArray(images),
+    widths: getArray(widths),
+    heights: getArray(heights),
+    username: (username as any) || "",
+    alerts: alerts || "",
+  };
+  parsedRequest.images = getDefaultImages(
+    parsedRequest.images,
+    parsedRequest.theme
+  );
+  return parsedRequest;
 }
 
 function getArray(stringOrArray: string[] | string | undefined): string[] {
-    if (typeof stringOrArray === 'undefined') {
-        return [];
-    } else if (Array.isArray(stringOrArray)) {
-        return stringOrArray;
-    } else {
-        return [stringOrArray];
-    }
+  if (typeof stringOrArray === "undefined") {
+    return [];
+  } else if (Array.isArray(stringOrArray)) {
+    return stringOrArray;
+  } else {
+    return [stringOrArray];
+  }
 }
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
-    const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+  const defaultImage =
+    theme === "light"
+      ? "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg"
+      : "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg";
 
-    if (!images || !images[0]) {
-        return [defaultImage];
-    }
+  if (!images || !images[0]) {
+    return [defaultImage];
+  }
 
-    return images;
+  return images;
 }
--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -1,17 +1,19 @@
-export type FileType = 'png' | 'jpeg';
-export type Theme = 'light';
+export type FileType = "png" | "jpeg";
+export type Theme = "light";
 
 export interface ParsedRequest {
-    fileType: FileType;
-    text: string;
-    theme: Theme;
-    md: boolean;
-    price: any;
-    marketcap: any;
-    h24: any;
-    l24: any;
-    change_24h: any;
-    images: string[];
-    widths: string[];
-    heights: string[];
+  fileType: FileType;
+  text: string;
+  theme: Theme;
+  md: boolean;
+  price: any;
+  marketcap: any;
+  h24: any;
+  l24: any;
+  username: string;
+  change_24h: any;
+  images: string[];
+  widths: string[];
+  heights: string[];
+  alerts: any;
 }
